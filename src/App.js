25aff800c5e2a0e588c87a3f0546c8cc00656173
Werayootk <werayoot5800@gmail.com>
2021-08-13 09:46:38 +0700

    Starter project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ReducerCase1 from './useStateCase1/StateCase1';
+
+function App() {
+  return (
+    <div className="App">
+      <ReducerCase1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/useReducerCase1/ReducerCase1.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ReducerCase1 = () => {
+    return <div></div>
+};
+
+export default ReducerCase1;
